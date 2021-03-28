7c9d5094ec3c30e76b339de1e220173b49ad5888
TraianEmanuel <81159716+TraianEmanuel@users.noreply.github.com>
2021-03-28 17:23:35 +0200

    Update MeteoapiApplicationTests.java

--- src/test/java/it/univpm/traianubertinivisi/meteoapi/MeteoapiApplicationTests.java
+++ src/test/java/it/univpm/traianubertinivisi/meteoapi/MeteoapiApplicationTests.java
@@ -1,13 +1,45 @@
 package it.univpm.traianubertinivisi.meteoapi;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import it.univpm.traianubertinivisi.meteoapi.controllers.HomeRestController;
+import it.univpm.traianubertinivisi.openweather.city.City;
 
 @SpringBootTest
 class MeteoapiApplicationTests {
 
+	@Autowired
+	HomeRestController homeRestController;
+	
+	
 	@Test
-	void contextLoads() {
+	void nameTest (){
+		City test = new City(); 
+		 test.setName("Macerata");
+		assertEquals("Macerata", test.toString());
 	}
 
-}
+	@Test
+	void homeTest() {
+		assertTrue(this.homeRestController.home() instanceof String);
+	}
+
+	@Test
+	void citiesLoadTest() {
+		assertTrue(this.homeRestController.citiesLoad("00980018-f515-4390-9961-d865c629bcf9") instanceof String);
+	}
+
+	@Test
+	void citiesStopLoadTest() {
+		assertTrue(this.homeRestController.citiesStopLoad("00980018-f515-4390-9961-d865c629bcf9") instanceof String);
+	}
+
+	@Test
+	void citiesTest() throws Exception {
+		assertTrue(this.homeRestController.cities("Macerata","It") instanceof List<?>);
+	}
+
+  }
